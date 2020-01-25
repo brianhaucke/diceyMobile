50d49065fd8794ec16597c262b4aa3192c9f5a71
brianhaucke <brian.haucke@gmail.com>
2020-01-24 16:16:54 -0800

    It's Yahtzee now
    Add more dice, add ability to hold dice you want, dice stop rolling after 3 rolls, reset button to roll again

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,21 +14,86 @@ class ViewController: UIViewController {
     // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView3: UIImageView!
+    @IBOutlet weak var diceImageView4: UIImageView!
+    @IBOutlet weak var diceImageView5: UIImageView!
     
+    var diceRolls = 0
+    var isButton1Pressed = false
+    var isButton2Pressed = false
+    var isButton3Pressed = false
+    var isButton4Pressed = false
+    var isButton5Pressed = false
+    
+
+    // When dice are tapped, don't roll them again
+
+    @IBAction func button1Pressed(_ sender: UIButton) {
+        isButton1Pressed = true
+    }
+    @IBAction func button2Pressed(_ sender: UIButton) {
+        isButton2Pressed = true
+    }
+    @IBAction func button3Pressed(_ sender: UIButton) {
+        isButton3Pressed = true
+    }
+    @IBAction func button4Pressed(_ sender: UIButton) {
+        isButton4Pressed = true
+    }
+    @IBAction func button5Pressed(_ sender: UIButton) {
+        isButton5Pressed = true
+    }
     // When the 'Roll' button gets tapped
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        
+        diceRolls+=1
+        print("`Roll` Button got tapped.")
+        print(diceRolls)
         // Array of possible dice rolls - image Literals
         let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
         
         //Select a random element from the diceArray array
-        diceImageView1.image = diceArray.randomElement()
-        diceImageView2.image = diceArray.randomElement()
         //diceImageView2.image =  diceArray[Int.random(in:0...5)]
         
-        
-
+        if (isButton1Pressed == false){
+        diceImageView1.image = diceArray.randomElement()
+        }
+        if (isButton2Pressed == false){
+        diceImageView2.image = diceArray.randomElement()
+        }
+        if (isButton3Pressed == false){
+        diceImageView3.image = diceArray.randomElement()
+        }
+        if (isButton4Pressed == false){
+        diceImageView4.image = diceArray.randomElement()
+        }
+        if (isButton5Pressed == false){
+        diceImageView5.image = diceArray.randomElement()
+        }
+        if (diceRolls == 3){
+            isButton1Pressed = true
+            isButton2Pressed = true
+            isButton3Pressed = true
+            isButton4Pressed = true
+            isButton5Pressed = true
+        }
     }
+        
+    // When the Reset button is tapped, reset all dice and un-hold them
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        diceRolls = 0
+        print("Dice rolls reset to ", diceRolls)
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView3.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView4.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView5.image = #imageLiteral(resourceName: "DiceOne")
+        isButton1Pressed = false
+        isButton2Pressed = false
+        isButton3Pressed = false
+        isButton4Pressed = false
+        isButton5Pressed = false
+    }
+    
+    
 }
 
